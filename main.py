d0a260bae158334d795dafadf145080f239c2249
Semilore Idowu <semijacks27@gmail.com>
2022-09-19 14:57:49 +0100

    basics

---
+++ main.py
@@ -0,0 +1,7 @@
+temperature = 98.6
+original = temperature
+
+temperature = temperature + 5.0
+
+print(temperature)
+print(original)
---
+++ types.py
@@ -0,0 +1,11 @@
+names = ['Semilore', 'Edward', 'John']
+
+# type() returns data type class
+print(type(names))
+
+numbers = tuple(range(10))
+
+print(numbers)
+
+numbers = tuple(range(20))
+print(numbers)
